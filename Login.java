c39d420653d098b9188b3414ce84d31c7855cb26
luciusfsilva <lucianoferreiraofficial@gmail.com>
2025-12-13 21:50:36 -0300

    feat: adicionado login Develop

--- Login.java
+++ Login.java
@@ -1,5 +1,10 @@
 public Class Login {
 	public static void main(String [] args) {
 		System.out.println("Login atual")
+		loginDevelop();
+	}
+	
+	public static void loginDevelop() {
+		System.out.println("Login develop")
 	}
 }
