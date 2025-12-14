09857225030065e1d028f6ff30ea2d304860e043
luciusfsilva <lucianoferreiraofficial@gmail.com>
2025-12-13 21:17:17 -0300

    feat: inclusão de teste

--- CadastroService.java
+++ CadastroService.java
@@ -2,6 +2,11 @@ public class CadastroService {
 	
 	public static void main(String [] args) {
 		System.out.println("Cadastro de cliente de ana");
+		teste();
 	}
 	
+	public static void teste() {
+		if (true)
+		System.out.print("Entrei no verdadeiro")
+	}
 }
