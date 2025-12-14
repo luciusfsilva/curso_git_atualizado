3f47b5a81e3e953da66d309018dd4cf5a07bdd07
luciusfsilva <lucianoferreiraofficial@gmail.com>
2025-12-13 21:48:22 -0300

    feat: inclusão login generico

--- Login.java
+++ Login.java
@@ -1,5 +1,10 @@
 public Class Login {
 	public static void main(String [] args) {
 		System.out.println("Login atual")
+		loginGenerico();
+	}
+	
+	public static void loginGenerico() {
+		System.out.println("Login generico")
 	}
 }
