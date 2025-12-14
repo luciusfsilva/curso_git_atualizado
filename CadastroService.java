0f74e45a0c6dbbc7d3e75013d6136b57d4120d78
luciusfsilva <lucianoferreiraofficial@gmail.com>
2025-12-13 21:19:57 -0300

    feat: inclusao teste do carlos

--- CadastroService.java
+++ CadastroService.java
@@ -2,6 +2,12 @@ public class CadastroService {
 	
 	public static void main(String [] args) {
 		System.out.println("Cadastro de cliente de ana");
+		testePRD();
 	}
 	
+	public static void testePRD() {
+		boolean falso = false;
+		if (falso) 
+			System.out.println("Testando falso");
+	}
 }
